1d05e75c46a6dde4f5cebcb7617d1ff2b4c96121
Lea Karimi <107368207+leabea@users.noreply.github.com>
2023-05-26 12:34:53 +0200

    Update webanwendung.py

--- webanwendung.py
+++ webanwendung.py
@@ -998,8 +998,9 @@ def update_output(selected_graph, periods, list_of_contents, list_of_names):
                 column_index = 0
                 column_name = column_names[column_index]
 
-                df2.iloc[:, [column_index]] = pd.to_datetime(df2.iloc[:, [column_index]].astype(str).agg('-'.join, axis=1),
-                                                             format='%Y-%m-%d')
+                #df2.iloc[:, [column_index]] = pd.to_datetime(df2.iloc[:, [column_index]].astype(str).agg('-'.join, axis=1),
+                #  format='%Y-%m-%d')
+                df2.iloc[:, [column_index]] = pd.to_datetime(df2.iloc[:, [column_index]].astype(str).apply('-'.join, axis=1), format='%Y-%m-%d')
                 
                 df2 = df2.set_index(column_name)
                 #Quelle: Hirschle, J. (2021): Machine Learning für Zeitreihen: Einstieg in Regressions-, ARIMA-und Deep Learning-Verfahren mit Python Inkl. E-Book, 1. Auflage, Carl Hanser Verlag GmbH Co. KG, München, S. 118.
@@ -1157,7 +1158,8 @@ def update_output(selected_graph, periods, list_of_contents, list_of_names):
                 column_name = column_names[column_index]
 
                 #erste Spalte des Dataframe in das gewünschte Datumsformat umwandeln
-                df2.iloc[:, [column_index]] = pd.to_datetime(df2.iloc[:, [column_index]].astype(str).agg('-'.join, axis=1), format='%Y-%m-%d')
+                #df2.iloc[:, [column_index]] = pd.to_datetime(df2.iloc[:, [column_index]].astype(str).agg('-'.join, axis=1), format='%Y-%m-%d')
+                df2.iloc[:, [column_index]] = pd.to_datetime(df2.iloc[:, [column_index]].astype(str).apply('-'.join, axis=1), format='%Y-%m-%d')
                 
                 df2 = df2.set_index(column_name)
                 #Quelle: Hirschle, J. (2021): Machine Learning für Zeitreihen: Einstieg in Regressions-, ARIMA-und Deep Learning-Verfahren mit Python Inkl. E-Book, 1. Auflage, Carl Hanser Verlag GmbH Co. KG, München, S. 118.
